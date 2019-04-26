775fcf707d2df9d44f15c013547af6241a6813c9
Jevgeni Pillau <angleter@gmail.com>
2019-04-26 07:57:40 +0300

    Homework for lesson02 Django01 course (by JevgeniPillau)

--- omnifood_prj/mainapp/views.py
+++ omnifood_prj/mainapp/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def main(request):
-    return render(request, 'mainapp/index.html') 
+    return render(request, 'mainapp/index.html')
+
+
+def base(request):
+    return render(request, 'mainapp/base.html')
+
 
 def products(request):
-    return render(request, 'mainapp/products.html') 
+    context = {'products': ['apple', 'banana', 'kiwi']}
+    return render(request, 'mainapp/products.html', context)
+
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html') 
+    return render(request, 'mainapp/contacts.html')
